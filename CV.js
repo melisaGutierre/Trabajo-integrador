8eb4b9a96cc15134b57f9824dcb1eac5e26a0f8d
melisaGutierre <126634672+melisaGutierre@users.noreply.github.com>
2023-03-01 14:25:58 -0300

    Add files via upload

---
+++ CV.js
@@ -0,0 +1,8 @@
+document.getElementById ('boton').onclick = function () {
+    console.log ("capturamos el evento click");
+    document.getElementById ("demo").innerHTML="terrancecontabilidad.com.ar"
+}
+document.getElementById ('boton2').onclick = function () {
+    console.log ("capturamos el evento click");
+    document.getElementById ("demo2").innerHTML= "Gerente Fashionnissima: Henry Cooper, Tel.(850) 500-3980  "
+}
